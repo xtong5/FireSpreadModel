3533970f4abe74f07ce2efe4bc12397ea68a2a37
Xin Tong <xtong5@hawk.iit.edu>
2022-03-14 13:09:29 -0400

    add ember wash

---
+++ Ember Wash/fireLine.m
@@ -0,0 +1,222 @@
+clear
+
+addpath src_ember_wash
+
+usePlot = true;
+saveVideo = true;
+
+if saveVideo
+    v = VideoWriter('test_ember_wash','MPEG-4');
+    v.FrameRate = 2;
+    open(v)
+end
+
+
+% velocity scale is 1m/s
+%prams.V = 1;
+% length scale is 1m (typical size of a single pixel)
+prams.L = 1;
+
+% number of points in x and y direction. Also sets the domain size
+prams.N = 201; 
+prams.dx = 1; % dimensionless size of a cell
+
+prams.uwind = 4; % dimensionless speed of wind from the south
+prams.s = prams.uwind/4; 
+% dimensionless speed of the fire spread rate due only to wind
+
+% 2x uwind produces lateral spread effects strength of omega in Sharples
+% paper = 0.3 (scaling?)
+prams.vortStrength = 0.5*prams.uwind/prams.dx^2; % strength of the vorticity
+%prams.vortStrength = -2*prams.s/prams.dx^2; % strength of the vorticity
+
+% 0.9 base runs %4. %4. %0.3; 1x uwind has large effect. Adding 1 causes
+% blow-up at uwind = 5 strength of nu in Sharples paper. The minus sign
+% to make it a sink is accounted for later in the code
+%prams.sinkStrength = (0.2*(exp(-prams.uwind) + 0.8*prams.uwind))/...
+%  prams.dx^2;
+prams.sinkStrength = 0.7; % for uwind = 1
+%prams.sinkStrength = 0.23; % for uwind = 1
+%prams.sinkStrength = 0.35; % for uwind = 2
+
+%prams.vortStrength = 0;
+%prams.sinkStrength = 0;
+
+% time required for cell to travel 3 grid points
+if prams.s ~= 0
+  prams.dt = 3*prams.dx/prams.s; 
+else
+  prams.dt = 2.0;
+%  prams.dt = 4;
+end
+prams.T = 300;
+prams.ntime = ceil(prams.T/prams.dt);
+
+% Time steps that cell can burn for ...in zero windXX make ~ 1/wind
+% number of dimensionless time steps before a cell burns out
+flameOutTime = 15; % time to flameout in seconds
+prams.flameOut = ceil(flameOutTime/prams.dt); 
+
+% probability that a cell ignites a neighbor to model diffusion.
+% Probabilities of igniting diagonal neighbors is scaled by sqrt(2) to
+% account for additional distance. Ignition due to diffusion only occurs
+% just before a cell burns out
+prams.probIgnite = 4e-1;  
+
+% create object to store information (state, burn time, flame out,
+% velocity) of each cell
+cells = geom(prams);
+
+% create ignition pattern
+% cells.initialState('junction30');
+cells.initialState('spot');
+% state = 0 => Cell has no fuel (either burnt or never existed)
+% state = 1 => Cell is not on fire but has reamining fuel
+% state = 2 => Cell is currently on fire
+
+% black out the boundary
+cells.blackout;
+
+% space to save the state at each time step
+state = zeros(prams.N,prams.N,prams.ntime+1);
+velx = zeros(prams.N,prams.N,prams.ntime+1);
+vely = zeros(prams.N,prams.N,prams.ntime+1);
+
+% object for the PDE solvers and the Bresenham algorithm
+os = solvers(prams);
+
+% current time
+time = 0;
+
+streams = true;
+xstart = linspace(22,179,100);
+ystart = 22*ones(size(xstart));
+
+%% first step
+i = 1;
+disp(time)
+sinkForce = os.sinkTerm(cells);
+vortForce = os.vortTerm(cells);
+[psi,psix,psiy] = os.PoissonSolverNeumann(sinkForce); 
+[psi,etax,etay] = os.PoissonSolverNeumann(+vortForce);
+cos = zeros(prams.N,prams.N); sin = cos;
+[cells.velx,cells.vely] = os.computeVelocity(...
+        psix,psiy,etax,etay,cos,sin);
+state(:,:,i) = cells.state;
+velx(:,:,i) = cells.velx;
+vely(:,:,i) = cells.vely;
+i = i + 1;
+time = time + prams.dt;
+
+while time < prams.T
+  disp(time)
+% If there is a prescribed number of time steps
+%for i = 1:prams.ntime
+  % sink and vorticity terms due to cells that are on fire. Both are
+  % divided by dx to account for the fact that the (x,y) divergence is
+  % dw/dz
+  sinkForce = os.sinkTerm(cells);
+  vortForce = os.vortTerm(cells);
+
+  % compute velocity due to sinks
+  [psi,psix,psiy] = os.PoissonSolverNeumann(sinkForce); 
+%  [psi,psix,psiy] = os.PoissonSolverDirichlet(sinkForce); 
+
+  % compute velocity due to vorticity
+  [psi,etax,etay] = os.PoissonSolverNeumann(+vortForce);
+%  [psi,etax,etay] = os.PoissonSolverDirichlet(+vortForce);
+
+  % add the different terms in the velocity
+  hypo = (velx(:,:,i-1).^2+vely(:,:,i-1).^2).^(0.5);
+  cos = velx(:,:,i-1)./hypo; sin = vely(:,:,i-1)./hypo; 
+  [cells.velx,cells.vely] = os.computeVelocity(...
+        psix,psiy,etax,etay,cos,sin);
+
+  state(:,:,i) = cells.state;
+  velx(:,:,i) = cells.velx;
+  vely(:,:,i) = cells.vely;
+  i = i + 1;
+
+  % visualize the current state
+  if usePlot
+    cells.vis(time,1,streams,xstart,ystart);
+    if saveVideo
+        frame = getframe(gcf);
+        writeVideo(v,frame);
+    end
+  end
+%  fprintf('time step %i! PAUSED\n',i)
+  % update time
+  time = time + prams.dt;
+  pause(.01)
+%  pause
+
+%  figure(2);
+%  os.FormStreams(cells,xstart,ystart);
+ figure(1)
+%  pause
+
+  % update the state of the cells and the amount of time they've been
+  % burning
+  os.updateState(cells);
+end
+
+cx = cells.cx; cy = cells.cy;
+state(:,:,prams.ntime+1) = cells.state;
+velx(:,:,prams.ntime+1) = cells.velx;
+vely(:,:,prams.ntime+1) = cells.vely;
+
+% visualize the final state
+% if usePlot
+%   cells.vis(time,1,streams,xstart,ystart);
+% end
+% pause(.01)
+
+cells = geom(prams);
+for k = 1:prams.ntime+1
+ cells.state = state(:,:,k);
+ cells.velx = velx(:,:,k);
+ cells.vely = vely(:,:,k);
+ time = (k-1)*prams.dt;
+ cells.vis(time);
+ pause(0.01)
+end
+%save('fireline.mat','prams','state','velx','vely');
+
+fat = cells.fat(state,prams.dt);
+
+% x0 = 45; y0 = 42;
+% %x1 = 102; y1 = 69;
+% x1 = 160; y1 = 96;
+
+x0 = 100; y0 = 40;
+x1 = 100; y1 = 80;
+
+[x,y,fatVec] = cells.fatVec(fat,x0,y0,x1,y1);
+
+% figure(3); clf;
+% plot(y,fatVec);
+
+% save('fireData.mat','cx','cy','fat','fatVec','prams','x','y','xstart','ystart');
+
+figure(2); clf;
+titleStr = ['First Arrival Time'];
+title(titleStr,'fontsize',20)
+surface(cells.dx*(cells.cx-0.5)*cells.L,...
+        cells.dx*(cells.cy-0.5)*cells.L,(fat));
+view(2);
+shading flat;
+colorbar
+cmap = buildcmap('kbryw');
+colormap(cmap)
+axis equal
+axis([0 cells.dx*cells.N*cells.L 0 cells.dx*cells.N*cells.L])
+set(gca,'fontsize',15)
+xlabel('meters','fontsize',16)
+ylabel('meters','fontsize',16)
+
+if saveVideo
+    frame = getframe(gcf);
+    writeVideo(v,frame);
+    close(v)
+end
---
+++ Ember Wash/src_ember_wash/solvers.m
@@ -0,0 +1,337 @@
+classdef solvers
+% different PDE solvers
+
+
+properties
+N
+dx
+dt
+uwind
+s
+sinkStrength
+vortStrength
+probIgnite
+L
+
+end % properties
+
+methods
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function o = solvers(prams)
+
+o.N = prams.N;
+o.dx = prams.dx;
+o.dt = prams.dt;
+o.uwind = prams.uwind;
+o.s = prams.s;
+o.sinkStrength = prams.sinkStrength;
+o.vortStrength = prams.vortStrength;
+o.probIgnite = prams.probIgnite;
+o.L = o.PoissonSolverNeumannMatrix;
+
+
+end % solvers: constructor
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function sink = sinkTerm(o,cells)
+
+sink = zeros(cells.N,cells.N);
+sink(cells.state == 2) = -o.sinkStrength;
+
+end % sinkTerm
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function vort = vortTerm(o,grid)
+
+vort = zeros(grid.N,grid.N);
+
+% search along each row at cells that are burning. Set the point to the
+% right and left to be +1 and -1 so that it forms the desired vorticity
+for k = 1:grid.N
+  ind = find(grid.state(:,k) == 2);
+  if numel(ind) ~= 0
+    % For broken fire lines, this will put a positive vorticity just to
+    % the left and a negative vorticity just to the right of each
+    % connected component. There is no vorticity force inside the
+    % fireline
+    vort(ind-1,k) = vort(ind-1,k) + o.vortStrength;
+    vort(ind+1,k) = vort(ind+1,k) - o.vortStrength;
+    vort(ind,k) = 0;
+  end
+end
+
+% account for the fact that dw/dz depends on the grid scale (dz)
+%vort = vort/o.dx;
+
+end % vortTerm
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function L = PoissonSolverNeumannMatrix(o)
+% Compupte the matrix for 2D Poisson equation with Neumann boundary
+% conditions
+
+N = o.N;
+
+% II needed for top and bottom boundary conditions
+II = speye(N);
+% IIt needed for interaction between points above and below. It is zero
+% in the first and last rows because we are enforcing no flux in the
+% horizontal (normal) direction at these points
+IIt = II; IIt(1,1) = 0; IIt(end,end) = 0;
+
+% D is the matrix that handles diagonal terms and neighbors to the left
+% and right. First and last rows are zero since these are the 
+e = ones(N,1);
+D = spdiags([e -4*e e],(-1:1),N,N);
+D(1,1) = -1*o.dx; D(1,2) = 1*o.dx;
+D(N,N-1) = 1*o.dx; D(N,N) = -1*o.dx;
+
+L = spalloc(N^2,N^2,6*N);
+% Neumann boundary condition at bottom
+L(1:N,1:N) = -II/o.dx;
+L(1:N,N+1:2*N) = II/o.dx;
+% Neumann boundary condition at top
+L(N*(N-1)+1:end,N*(N-1)+1:end) = -II/o.dx;
+L(N*(N-1)+1:end,N*(N-2)+1:N*(N-1)) = II/o.dx;
+
+% points below
+for k = 2:N-1
+  icol = (k-2)*N+1;
+  irow = (k-1)*N+1;
+  L(irow:irow+N-1,icol:icol+N-1) = IIt/o.dx^2;
+end
+
+% points above
+for k = 2:N-1
+  icol = k*N+1;
+  irow = (k-1)*N+1;
+  L(irow:irow+N-1,icol:icol+N-1) = IIt/o.dx^2;
+end
+
+% diagonal and points to left and right
+for k = 2:N-1
+  icol = (k-1)*N+1;
+  irow = (k-1)*N+1;
+  L(irow:irow+N-1,icol:icol+N-1) = D/o.dx^2;
+end
+% Force the first term in psi to be 0
+%L = [L;speye(1,N^2)];
+
+% do LU decomposition so inversion can done efficiently
+%[L,U] = lu(L);
+
+end % PoissonSolverNeumannMatrix
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [psi,psi_x,psi_y] = PoissonSolverNeumann(o,f)
+% Solve the 2D Poisson equation using centered differences and
+% homogeneous Dirichlet boundary conditions
+
+N = size(f,1);
+psi_x = zeros(N);
+psi_y = zeros(N);
+
+% remove the edge terms since we are assuming a homogeneous Neumann
+% boundary condition
+f = f(2:end-1,2:end-1); 
+
+% choose the boundary data so that the compatability condition is
+% satisfied. Checked with Fredholm Alternative and this worked exactly
+% as we need (see lines below)
+bdFlux = +o.dx/(4*(N-2)) * sum(f(:));
+
+rhs = zeros(N^2,1);
+% put constant flux at all points on boundary
+rhs(1:N) = -bdFlux;
+rhs(N^2-N+1:N^2) = -bdFlux;
+rhs(N+1:N:N^2-2*N+1) = -bdFlux;
+rhs(2*N:N:N^2-N) = -bdFlux;
+M = size(f,1);
+for k = 1:M
+  rhs(N+(k-1)*N+2:N+(k-1)*N+M+1) = f(:,k);
+end
+%rhs = [rhs;0];
+psi = o.L\rhs;
+
+%clf
+%[V,D] = eig(full(o.L)');
+%[~,s] = min(abs(diag(D)));
+%V(:,s)'*rhs % by Fredholm Alternative, this number should be 0
+%plot(o.L*psi - rhs)
+%pause
+
+psi = reshape(psi,N,N);
+
+psi_x(2:end-1,2:end-1) = 0.5*(psi(3:end,2:end-1) - ...
+      psi(1:end-2,2:end-1))/o.dx;
+psi_y(2:end-1,2:end-1) = 0.5*(psi(2:end-1,3:end) - ...
+      psi(2:end-1,1:end-2))/o.dx;
+
+psi_x(1,:) = -bdFlux;
+psi_x(end,:) = +bdFlux;
+psi_y(:,1) = -bdFlux;
+psi_y(:,end) = +bdFlux;
+
+
+end % PoissonSolverNeumann
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [psi,psi_x,psi_y] = PoissonSolverDirichlet(o,f)
+% Solve the 2D Poisson equation using centered differences and
+% homogeneous Dirichlet boundary conditions
+
+N = size(f,1);
+% remove the edge terms since we are assuming a Dirichlet boundary
+% condition
+f = f(2:end-1,2:end-1); 
+
+e = ones(N-2,1);
+A = spdiags([e -2*e e],(-1:1),N-2,N-2);
+L = (kron(A,eye(N-2)) + kron(eye(N-2),A));
+
+psi = zeros(N);
+psi_x = zeros(N);
+psi_y = zeros(N);
+
+psi(2:end-1,2:end-1) = reshape(L\(o.dx^2*f(:)),N-2,N-2);
+psi_x(2:end-1,2:end-1) = 0.5*(psi(3:end,2:end-1) - ...
+      psi(1:end-2,2:end-1))/o.dx;
+psi_y(2:end-1,2:end-1) = 0.5*(psi(2:end-1,3:end) - ...
+      psi(2:end-1,1:end-2))/o.dx;
+
+end % PoissonSolver
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [velx,vely] = computeVelocity(o,psix,psiy,etax,etay,cos,sin)
+
+rdbino = binornd(1,0.01,o.N,o.N);
+velx = psix + etay + cos.*exprnd(o.s,o.N,o.N).*rdbino;
+vely = psiy - etax + o.s + sin.*exprnd(o.s,o.N,o.N).*rdbino;
+% velx = psix + etay + exprnd(10*o.s,o.N,o.N);
+% vely = psiy - etax + o.s + exprnd(10*o.s,o.N,o.N); %v*arrival time 
+
+end % computeVelocity
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function updateState(o,cells)
+
+% index of points currently on fire
+[FireIndx,FireIndy] = find(cells.state == 2);
+
+% update burn time of combusting cells
+for k = 1:numel(FireIndx)
+  cells.burnTime(FireIndx(k),FireIndy(k)) = ...
+        cells.burnTime(FireIndx(k),FireIndy(k)) + 1;
+end
+
+NewFireIndx = [];
+NewFireIndy = [];
+for k = 1:numel(FireIndx)
+  ii = FireIndx(k); jj = FireIndy(k);
+  % Compute index of points along the Bresenham line starting at burning
+  % cells and going in direction of velocity vector
+  [indx,indy] = cells.bham(cells.cx(ii,jj),cells.cy(ii,jj),...
+        o.dt/o.dx*cells.velx(ii,jj),o.dt/o.dx*cells.vely(ii,jj));
+
+%  [indx-0.0 indy-0.0]
+%  pause
+
+  % first point is always the cell itself, so don't need to consider
+  indx = indx(2:end); % cells along bresenham line 
+  indy = indy(2:end); % cells along bresenham line 
+
+  % Ignite along the Bresenham line but stop as soon as you reach a cell
+  % that is either already burning or burnt out
+  j = 1;
+  while (j <= numel(indx) && ...
+         indx(j) > 0 && indx(j) < cells.N && ...
+         indy(j) > 0 && indy(j) < cells.N && ...
+         cells.state(indx(j),indy(j)) == 1)
+    % Save indicies of points that will be ignited
+    NewFireIndx = [NewFireIndx indx(j)];
+    NewFireIndy = [NewFireIndy indy(j)];
+    j = j + 1;
+  end
+
+%  for j = 1:numel(indx)
+%    % Save indicies of points that will be ignited
+%    if (indx(j) > 0 && indx(j) < cells.N && ...
+%        indy(j) > 0 && indy(j) < cells.N && ...
+%        cells.state(indx(j),indy(j)) == 1)
+%      NewFireIndx = [NewFireIndx indx(j)];
+%      NewFireIndy = [NewFireIndy indy(j)];
+%%      [ii jj indx(j) indy(j)]
+%%      pause
+%    end
+%  end
+
+  if cells.burnTime(ii,jj) == cells.flameOut
+    % START OF DIFFUSION MODEL
+    % diffuse cells to the east
+    if (cells.state(ii+1,jj) == 1 && rand < o.probIgnite) 
+  %    disp('east')
+      NewFireIndx = [NewFireIndx ii+1];
+      NewFireIndy = [NewFireIndy jj+0];
+    end
+    % diffuse cells to the west
+    if (cells.state(ii-1,jj) == 1 && rand < o.probIgnite) 
+  %    disp('west')
+      NewFireIndx = [NewFireIndx ii-1];
+      NewFireIndy = [NewFireIndy jj+0];
+    end
+    % diffuse cells to the north
+    if (cells.state(ii,jj+1) == 1 && rand < o.probIgnite) 
+  %    disp('north')
+      NewFireIndx = [NewFireIndx ii+0];
+      NewFireIndy = [NewFireIndy jj+1];
+    end
+    % diffuse cells to the south
+    if (cells.state(ii,jj-1) == 1 && rand < o.probIgnite) 
+  %    disp('south')
+      NewFireIndx = [NewFireIndx ii+0];
+      NewFireIndy = [NewFireIndy jj-1];
+    end
+    % diffuse cells to the northeast
+    if (cells.state(ii+1,jj+1) == 1 && rand*sqrt(2) < o.probIgnite) 
+  %    disp('northeast')
+      NewFireIndx = [NewFireIndx ii+1];
+      NewFireIndy = [NewFireIndy jj+1];
+    end
+    % diffuse cells to the northwest
+    if (cells.state(ii-1,jj+1) == 1 && rand*sqrt(2) < o.probIgnite) 
+  %    disp('northwest')
+      NewFireIndx = [NewFireIndx ii-1];
+      NewFireIndy = [NewFireIndy jj+1];
+    end
+    % diffuse cells to the southeast
+    if (cells.state(ii+1,jj-1) == 1 && rand*sqrt(2) < o.probIgnite) 
+  %    disp('southeast')
+      NewFireIndx = [NewFireIndx ii+1];
+      NewFireIndy = [NewFireIndy jj-1];
+    end
+    % diffuse cells to the southwest
+    if (cells.state(ii-1,jj-1) == 1 && rand*sqrt(2) < o.probIgnite) 
+  %    disp('southwest')
+      NewFireIndx = [NewFireIndx ii-1];
+      NewFireIndy = [NewFireIndy jj-1];
+    end
+    % END OF DIFFUSION MODEL
+  end
+end
+
+% Ignite all new cells
+for k = 1:numel(NewFireIndx)
+  cells.state(NewFireIndx(k),NewFireIndy(k)) = 2;
+end
+
+% burn out cells that have been burning for the maximum number of
+% time steps.
+cells.state(cells.burnTime == cells.flameOut) = 0;
+
+end % updateState
+
+end % methods
+
+end % classdef
